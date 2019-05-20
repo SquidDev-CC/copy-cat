8ee274d7e3397d1a9282b51d849f95bade9f6822
SquidDev <bonzoweb@hotmail.co.uk>
2019-05-20 14:41:38 +0100

    Add file uploading too
    Allows uploading single files, and entire directories. Probably a little
    derpy with handling duplicates, but it's a nice start.

--- src/web/ts/computer/index.tsx
+++ src/web/ts/computer/index.tsx
@@ -33,12 +33,14 @@ type ComputerState = {
   id: number,
   label: string | null,
   on: boolean,
+
+  dragging: boolean,
 };
 
 const createZip = async (computer: ComputerAccess) => {
   const zip = await newZip();
 
-  const queue = [ "" ];
+  const queue = [""];
   while (true) {
     const path = queue.pop();
     if (path === undefined) break;
@@ -71,6 +73,8 @@ export class Computer extends Component<ComputerProps, ComputerState> {
       activeFile: null,
 
       id: 0, on: false, label: computer.getLabel(),
+
+      dragging: false,
     };
   }
 
@@ -84,20 +88,21 @@ export class Computer extends Component<ComputerProps, ComputerState> {
 
   public shouldComponentUpdate(
     { focused, settings }: Readonly<ComputerProps>,
-    { id, label, on, activeFile: activeFile }: Readonly<ComputerState>,
+    { id, label, on, activeFile: activeFile, dragging }: Readonly<ComputerState>,
   ): boolean {
     return focused !== this.props.focused || settings !== this.props.settings ||
       id !== this.state.id || label !== this.state.label || on !== this.state.on ||
-      activeFile !== this.state.activeFile;
+      activeFile !== this.state.activeFile || dragging !== this.state.dragging;
   }
 
   public render(
     { settings, focused }: ComputerProps,
-    { terminal, terminalChanged, computer, activeFile, id, label, on }: ComputerState,
+    { terminal, terminalChanged, computer, activeFile, id, label, on, dragging }: ComputerState,
   ) {
     return <div class="computer-view">
       <div class="computer-split">
-        <div class="file-list">
+        <div class={`file-list ${dragging ? "dragging" : ""}`}
+          onDragOver={this.startDrag} onDragLeave={this.stopDrag} onDrop={this.dropFile}>
           <div class="file-computer-control">
             <div class={`file-computer ${activeFile == null ? "active" : ""}`} onClick={this.openComputer}>
               {id ? `Computer #${id}` : "Computer"}
@@ -112,6 +117,10 @@ export class Computer extends Component<ComputerProps, ComputerState> {
 
           <FileTree computer={computer} entry={computer.getEntry("")!} path=""
             opened={activeFile === null ? null : activeFile.path} open={this.openFile} />
+
+          <div class="file-drop-marker">
+            <span>Upload to your computer!</span>
+          </div>
         </div>
         {activeFile == null
           ? <Terminal terminal={terminal} changed={terminalChanged} focused={focused}
@@ -120,6 +129,65 @@ export class Computer extends Component<ComputerProps, ComputerState> {
             doSave={contents => activeFile.file.setContents(contents)} />}
       </div>
     </div>;
+  }
+
+  private addOneFile(name: string, contents: string) {
+    const index = name.lastIndexOf(".");
+    const prefix = index > 0 ? name.substring(0, index) : name;
+    const suffix = index > 0 ? name.substring(index) : "";
+
+    // Add a number until we find a unique file. Or just give up at 100.
+    const computer = this.state.computer;
+    for (let i = 0; i < 100; i++) {
+      const uniqueName = i === 0 ? name : `${prefix}.${i}${suffix}`;
+      if (computer.getEntry(uniqueName)) continue;
+
+      const result = this.state.computer.createFile(uniqueName);
+      if (!result.value) continue;
+
+      result.value.setContents(contents);
+      break;
+    }
+  }
+
+  private addFile(file: File): void {
+    if (file.name.endsWith(".zip")) {
+      // We attempt to unpack a zip file into a folder with the same name.
+      newZip()
+        .then(async zip => {
+          await zip.loadAsync(file);
+
+          const computer = this.state.computer;
+
+          const zipName = file.name.substring(0, file.name.length - 4);
+          let dirName;
+          for (let i = 0; i < 100; i++) {
+            dirName = i === 0 ? zipName : `${zipName}.${i}`;
+            if (computer.getEntry(dirName)) continue;
+
+            const result = this.state.computer.createDirectory(dirName);
+            if (result.value) break;
+          }
+
+          for (const fileName in zip.files) {
+            if (!zip.files.hasOwnProperty(fileName)) continue;
+
+            let fullName = `${dirName}/${fileName}`;
+            const entry = zip.files[fileName];
+            if (entry.dir) {
+              if (fullName.endsWith("/")) fullName = fullName.substring(0, fullName.length - 1);
+              computer.createDirectory(fullName);
+            } else {
+              this.addOneFile(fullName, await entry.async("text"));
+            }
+          }
+        })
+        .catch(e => console.error(e));
+    } else {
+      const reader = new FileReader();
+      reader.onload = () => this.addOneFile(file.name, reader.result as string);
+      reader.readAsText(file);
+    }
   }
 
   private openFile = (path: string, file: FileSystemEntry) => {
@@ -150,5 +218,34 @@ export class Computer extends Component<ComputerProps, ComputerState> {
     createZip(this.state.computer)
       .then(x => saveBlob("computer", "zip", x))
       .catch(err => console.error(err));
+  }
+
+  private startDrag = (e: DragEvent) => {
+    e.preventDefault();
+    if (!this.state.dragging) this.setState({ dragging: true });
+  }
+
+  private stopDrag = () => {
+    this.setState({ dragging: false });
+  }
+
+  private dropFile = (e: DragEvent) => {
+    e.preventDefault();
+    this.setState({ dragging: false });
+
+    if (!e.dataTransfer) return;
+
+    if (e.dataTransfer.items) {
+      const items = e.dataTransfer.items;
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        if (item.kind === "file") this.addFile(item.getAsFile()!);
+      }
+    } else {
+      const files = e.dataTransfer.files;
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < files.length; i++) this.addFile(files[i]);
+    }
   }
 }
