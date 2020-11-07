9c254c66453428a13ac6550f2b65cc7f0a37c761
SquidDev <git@squiddev.cc>
2020-11-07 12:29:25 +0000

    Make the embedding code a little more flexible
     - Return a promise of the computer by default.
     - Expose some Preact functions + computer component, for those who want
       to do something more fancy (i.e. me).

--- src/web/ts/embed.tsx
+++ src/web/ts/embed.tsx
@@ -2,22 +2,24 @@ import { Component, h, render } from "preact";
 import { Semaphore, Terminal, TerminalData } from "@squid-dev/cc-web-term";
 import type { ConfigGroup } from "./classes";
 import { ComputerAccess } from "./computer/access";
-import { ComputerPersistance, StoragePersistence, VoidPersistence } from "./computer/persist";
+import { StoragePersistence, VoidPersistence } from "./computer/persist";
 import termFont from "@squid-dev/cc-web-term/assets/term_font.png";
 import termFontHd from "@squid-dev/cc-web-term/assets/term_font_hd.png";
 import requirejs from "require";
 
 type MainProps = {
-  font: string,
-  persistence: ComputerPersistance,
-  files: { [filename: string]: string | ArrayBuffer },
+  hdFont?: boolean | string,
+  persistId?: number,
+  files?: { [filename: string]: string | ArrayBuffer },
   label?: string,
   width?: number,
   height?: number,
+  resolve?: (computer: ComputerAccess) => void,
 }
 
 type MainState = {
   on: boolean,
+  font: string,
   label: string | null,
   terminal: TerminalData,
   terminalChanged: Semaphore,
@@ -30,23 +32,35 @@ const emptyGroup: ConfigGroup = {
   addInt: () => { },
 };
 
-class Main extends Component<MainProps, MainState> {
+class Computer extends Component<MainProps, MainState> {
   public constructor(props: MainProps, context: unknown) {
     super(props, context);
 
+    const { persistId, hdFont } = props;
+
     const terminal = new TerminalData();
     const terminalChanged = new Semaphore();
+
     const computer = new ComputerAccess(
-      props.persistence,
+      persistId === undefined ? new VoidPersistence() : new StoragePersistence(persistId),
       terminal, terminalChanged,
       (label, on) => this.setState({ label, on }),
     );
+    props.resolve?.(computer);
+
+    const font = typeof hdFont === "string" ? hdFont :
+      // We need to do some terrible path hackery to get this to resolve relative to the
+      // current script (and thus copy-cat.squiddev.ccc).
+      // termFont{Hd,} will be of the form "termFont_xxx.png" - we convert it to
+      // "./termFont_xxx.png", and then resolve.
+      requirejs.toUrl("./" + (hdFont === undefined || hdFont ? termFontHd : termFont));
 
     // Set up the file system from the list of files given.
-    for (const fileName in props.files) {
-      if (!Object.prototype.hasOwnProperty.call(props.files, fileName)) continue;
+    const files = props.files || {};
+    for (const fileName in files) {
+      if (!Object.prototype.hasOwnProperty.call(files, fileName)) continue;
 
-      const contents = props.files[fileName];
+      const contents = files[fileName];
       const file = computer.createFile(fileName);
       if (file.value === null) throw new Error(file.error);
 
@@ -57,7 +71,7 @@ class Main extends Component<MainProps, MainState> {
     this.setState({
       on: false,
       label: null,
-      terminal, terminalChanged, computer
+      font, terminal, terminalChanged, computer
     });
   }
 
@@ -65,30 +79,25 @@ class Main extends Component<MainProps, MainState> {
     this.state.computer.start(() => emptyGroup, this.props);
   }
 
-  public render({ font }: MainProps, { computer, terminal, terminalChanged, label, on }: MainState) {
+  public computerDidUnmount(): void {
+    this.state.computer.dispose();
+  }
+
+  public render({}: MainProps, { font, computer, terminal, terminalChanged, label, on }: MainState) {
     return <Terminal terminal={terminal} changed={terminalChanged} focused={true} computer={computer}
       font={font}
       id={0} label={label} on={on} />;
   }
 }
 
-export default (element: HTMLElement, options?: {
-  persistId?: number,
-  label?: string,
-  width?: number,
-  height?: number,
-  hdFont?: boolean | string,
-  files?: { [filename: string]: string | ArrayBuffer },
-}): void => {
-  const { persistId, hdFont, files } = options || {};
-
-  const persistence = persistId === undefined ? new VoidPersistence() : new StoragePersistence(persistId);
-  const font = typeof hdFont === "string" ? hdFont :
-    // We need to do some terrible path hackery to get this to resolve relative to the
-    // current script (and thus copy-cat.squiddev.ccc).
-    // termFont{Hd,} will be of the form "termFont_xxx.png" - we convert it to
-    // "./termFont_xxx.png", and then resolve.
-    requirejs.toUrl("./" + (hdFont === undefined || hdFont ? termFontHd : termFont));
-
-  render(<Main persistence={persistence} font={font} files={files || {}} {...options} />, element);
+const exported =  (element: HTMLElement, options?: MainProps): Promise<ComputerAccess> => {
+  return new Promise((resolve, _) =>
+    render(<Computer resolve={resolve} {...(options || {})} />, element));
 };
+
+export default exported;
+
+exported.h = h;
+exported.Component = Component;
+exported.render = render;
+exported.Computer = Computer;
