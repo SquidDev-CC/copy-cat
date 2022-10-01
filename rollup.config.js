b16796140c9df0c914dc5c17c18ad69a23d34e8e
Jonathan Coates <git@squiddev.cc>
2022-10-01 13:24:15 +0100

    Update some npm packages

--- rollup.config.js
+++ rollup.config.js
@@ -28,7 +28,7 @@ export default {
       preventAssignment: true,
 
       __storageBackend__: JSON.stringify(process.env.COPY_CAT_STORAGE || "storage"),
-      __monaco__: "https://cdn.jsdelivr.net/npm/monaco-editor@0.33.0",
+      __monaco__: "https://cdn.jsdelivr.net/npm/monaco-editor@0.34.0",
     }),
 
     postcss({
