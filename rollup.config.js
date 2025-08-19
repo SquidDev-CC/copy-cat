SquidDev-CC/copy-cat

commit 341c3681a6bf625fca41b77fc939f069bd2f51a1

Continued cleanup
 - Add most of our java.nio and java.io functionality to TeaVM, allowing
   us to reduce the number of patches we apply.

 - Rewrite some of the build script to be a little simpler.

 - Bump eslint, and fix so many rules.

rollup.config.js
@@ -1,16 +1,22 @@
-import path from "path";
 import { promises as fs } from "fs";
+import path from "path";
+import { createHash } from "crypto";
 
-import license from "rollup-plugin-license";
-import postcss from "rollup-plugin-postcss";
-import replace from "@rollup/plugin-replace";
 import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import terser from '@rollup/plugin-terser';
+import { minify as minifyJavascript } from "terser";
 import typescript from "@rollup/plugin-typescript";
 import url from "@rollup/plugin-url";
+import license from "rollup-plugin-license";
+import postcss from "rollup-plugin-postcss";
 
-const out = "build/rollup";
-
-export default {
+/**
+ * @param {string} out
+ * @param {boolean} minify
+ * @returns {import("rollup").RollupOptions}
+ */
+const makeSite = (out, minify) => ({
   input: ["src/web/ts/main.tsx", "src/web/ts/embed.tsx"],
   output: {
     dir: out,
@@ -18,22 +24,27 @@ export default {
     paths: {
       "monaco-editor": "vs/editor/editor.main",
     },
-    preferConst: true,
+    freeze: true,
+    generatedCode: {
+      constBindings: true,
+      arrowFunctions: true,
+    }
   },
   context: "window",
-  external: ["monaco-editor", "require", "jszip"],
+  external: ["monaco-editor", "require"],
 
   plugins: [
     replace({
       preventAssignment: true,
 
       __storageBackend__: JSON.stringify(process.env.COPY_CAT_STORAGE || "storage"),
-      __monaco__: "https://cdn.jsdelivr.net/npm/monaco-editor@0.34.0",
+      __monaco__: "https://cdn.jsdelivr.net/npm/monaco-editor@0.43.0",
     }),
 
     postcss({
       namedExports: true,
       modules: true,
+      minimize: minify,
     }),
     url({
       limit: 1024,
@@ -43,7 +54,8 @@ export default {
 
     typescript(),
     resolve({ browser: true, }),
-    // commonjs(),
+
+    minify && terser(),
 
     license({
       banner:
@@ -58,16 +70,36 @@ export default {
 
     {
       name: "copy-cat",
-      async writeBundle () {
-        await Promise.all([
-          fs.copyFile("node_modules/requirejs/require.js", `${out}/require.js`),
-          fs.copyFile("node_modules/jszip/dist/jszip.js", `${out}/jszip.js`),
-        ]);
+      async generateBundle(_, bundle) {
+        const contents = await fs.readFile("node_modules/requirejs/require.js", { encoding: "utf-8" });
+        this.emitFile({
+          type: "asset",
+          name: "require.js",
+          fileName: "require.js",
+          source: minify ? (await minifyJavascript(contents)).code : contents,
+        });
+
+        const version = createHash("sha256")
+          .update(bundle["main.js"].code)
+          .update(await fs.readFile(`src/web/public/main.css`))
+          .digest("hex").slice(0, 8);
+
+        await Promise.all(["index.html", "404.html", "main.css"].map(async x => this.emitFile({
+          type: "asset",
+          name: x,
+          fileName: x,
+          source: (await fs.readFile(`src/web/public/${x}`, { encoding: "utf-8" })).replaceAll("{{version}}", version),
+        })));
       },
-      async resolveId (source) {
+      async resolveId(source) {
         if (source === "./classes") return path.resolve("build/teaVM/classes.js");
         return null;
       },
     },
   ],
-};
+});
+
+export default [
+  makeSite("build/web", false),
+  makeSite("build/webMin", true),
+];

src/web/ts/computer/access.ts
@@ -1,5 +1,5 @@
-import { ComputerActionable, KeyCode, LuaValue, Semaphore, TerminalData, lwjgl3Code } from "@squid-dev/cc-web-term";
-import { ConfigFactory, ComputerAccess as IComputerAccess, FileSystemEntry as IFileSystemEntry, Result, start } from "../java";
+import { type ComputerActionable, type KeyCode, type LuaValue, Semaphore, TerminalData, lwjgl3Code } from "@squid-dev/cc-web-term";
+import { type ConfigFactory, type ComputerAccess as IComputerAccess, type FileSystemEntry as IFileSystemEntry, type Result, start } from "../java";
 import type { BasicAttributes, ComputerPersistance } from "./persist";
 import type { ComputerCallbacks, FileAttributes, PeripheralKind, Side } from "../classes";
 
@@ -20,18 +20,18 @@ const encoder = new TextEncoder();
 export class FileSystemEntry implements IFileSystemEntry {
   private readonly persistance: ComputerPersistance;
   private readonly path: string;
-  private children: string[] | null;
+  private children: Array<string> | null;
   private contents: Int8Array | null;
   private exists: boolean = true;
   private semaphore?: Semaphore;
   private attributes: BasicAttributes;
 
-  public constructor(persistance: ComputerPersistance, path: string, children: string[] | null, contents: Int8Array | null, attributes: BasicAttributes | null) {
+  public constructor(persistance: ComputerPersistance, path: string, children: Array<string> | null, contents: Int8Array | null, attributes: BasicAttributes | null) {
     this.persistance = persistance;
     this.path = path;
     this.children = children;
     this.contents = contents;
-    this.attributes = attributes === null ? { modification: 0, creation: 0 } : attributes;
+    this.attributes = attributes ?? { modification: 0, creation: 0 };
   }
 
   public static create(persistance: ComputerPersistance, path: string, directory: boolean): FileSystemEntry {
@@ -45,12 +45,12 @@ export class FileSystemEntry implements IFileSystemEntry {
     return this.children != null;
   }
 
-  public getChildren(): string[] {
+  public getChildren(): Array<string> {
     if (this.children === null) throw Error("Not a directory");
     return this.children;
   }
 
-  public setChildren(children: string[]): void {
+  public setChildren(children: Array<string>): void {
     if (this.children === null) throw Error("Not a directory");
     this.children = children;
     if (this.semaphore) this.semaphore.signal();
@@ -101,7 +101,7 @@ export class FileSystemEntry implements IFileSystemEntry {
   }
 
   public getSemaphore(): Semaphore {
-    return this.semaphore || (this.semaphore = new Semaphore());
+    return this.semaphore ?? (this.semaphore = new Semaphore());
   }
 
   public doesExist(): boolean {
@@ -200,7 +200,7 @@ export class ComputerAccess implements IComputerAccess, ComputerActionable {
   }
 
   public getEntry(path: string): FileSystemEntry | null {
-    return this.filesystem.get(path) || null;
+    return this.filesystem.get(path) ?? null;
   }
 
   public createDirectory(path: string): Result<FileSystemEntry> {
@@ -271,7 +271,7 @@ export class ComputerAccess implements IComputerAccess, ComputerActionable {
         this.handlers = computer;
         if (this.removed) computer.dispose();
 
-        const { width, height, label } = options || {};
+        const { width, height, label } = options ?? {};
         if (typeof width === "number" && typeof height === "number") computer.resize(width, height);
 
         if (typeof this.label === "string") computer.setLabel(this.label);
@@ -282,8 +282,8 @@ export class ComputerAccess implements IComputerAccess, ComputerActionable {
       .catch(e => console.error("Cannot start computer", e));
   }
 
-  public queueEvent(event: string, args: LuaValue[]): void {
-    if (this.handlers !== undefined) this.handlers.event(event, args.map(x => JSON.stringify(x)));
+  public queueEvent(event: string, args: Array<LuaValue>): void {
+    if (this.handlers !== undefined) this.handlers.event(event, args);
   }
 
   public keyDown(key: KeyCode, repeat: boolean): void {
@@ -313,7 +313,7 @@ export class ComputerAccess implements IComputerAccess, ComputerActionable {
     if (this.handlers) this.handlers?.dispose();
   }
 
-  public setPeripheral(side: Side, kind: PeripheralKind | null) {
+  public setPeripheral(side: Side, kind: PeripheralKind | null): void {
     if (this.handlers) {
       this.handlers.setPeripheral(side, kind);
     } else {

commit 35f4644ec84ce1900f597fa1faa3575652d89753

Fix jszip bundling
This has been broken since 341c3681a6bf625fca41b77fc939f069bd2f51a1, and
I guess nobody has noticed!

rollup.config.js
@@ -37,7 +37,7 @@ const makeSite = (out, minify) => ({
     }
   },
   context: "window",
-  external: ["monaco-editor", "require"],
+  external: ["monaco-editor", "require", "jszip"],
 
   plugins: [
     replace({
@@ -81,13 +81,20 @@ const makeSite = (out, minify) => ({
       },
 
       async generateBundle(_, bundle) {
-        const contents = await fs.readFile("node_modules/requirejs/require.js", { encoding: "utf-8" });
-        this.emitFile({
-          type: "asset",
-          name: "require.js",
-          fileName: "require.js",
-          source: minify ? (await minifyJavascript(contents)).code : contents,
-        });
+        for (const [input, output] of [
+          // This is the main entrypoint to our package
+          ["node_modules/requirejs/require.js", "require.js"],
+          // This is already bundled using UMD, so we can't pass it through rollup.
+          ["node_modules/jszip/dist/jszip.js", "jszip.js"],
+        ]) {
+          const contents = await fs.readFile(input, { encoding: "utf-8" });
+          this.emitFile({
+            type: "asset",
+            name: output,
+            fileName: output,
+            source: minify ? (await minifyJavascript(contents)).code : contents,
+          });
+        };
 
         const version = createHash("sha256")
           .update(bundle["main.js"].code)

src/web/ts/computer/access.ts
@@ -76,7 +76,7 @@ export class FileSystemEntry implements IFileSystemEntry {
     return decoder.decode(this.getContents());
   }
 
-  public setContents(contents: ArrayBuffer | string): Result<true> {
+  public setContents(contents: Int8Array | ArrayBuffer | string): Result<true> {
     if (this.children !== null) throw Error("Not a file");
     if (!this.exists) return { error: "File has been deleted", value: null };
     this.attributes.modification = Date.now();